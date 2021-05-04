32c7a3e6af854f30acaf101df1e0d197f565897b
HasibullahMahmood <HasibullahMahmood@gmail.com>
2021-05-04 20:57:58 +0300

    some changes

--- src/components/CustomSelect.js
+++ src/components/CustomSelect.js
@@ -3,20 +3,14 @@ import Select from 'react-select';
 
 class CustomSelect extends Component {
 	render() {
-		const {
-			selectedObj,
-			onSelect,
-			options,
-			className,
-			defaultValue,
-		} = this.props;
+		const { selectedObj, onSelect, options, className, defaultValue } = this.props;
 		return (
 			<Select
 				defaultValue={defaultValue}
 				value={selectedObj}
 				onChange={onSelect}
 				options={options}
-				noOptionsMessage="No Options"
+				noOptionsMessage={() => 'No Options'}
 				placeholder="Select..."
 				className={'select-default ' + className}
 				menuPortalTarget={document.querySelector('body')}
--- src/components/MultiSelect.js
+++ src/components/MultiSelect.js
@@ -1,25 +1,25 @@
-import React from "react";
-import Select from "react-select";
-import makeAnimated from "react-select/animated";
+import React from 'react';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
 
 const animatedComponents = makeAnimated();
 
 export default function AnimatedMulti(props) {
-  const { selectedObjects, onSelect, options, className } = props;
+	const { selectedObjects, onSelect, options, className } = props;
 
-  return (
-    <Select
-      closeMenuOnSelect={false}
-      components={animatedComponents}
-      isMulti
-      defaultValue={[options[0]]}
-      value={selectedObjects}
-      onChange={onSelect}
-      options={options}
-      noOptionsMessage="No Options"
-      placeholder="Select..."
-      className={"select-default " + className}
-      menuPortalTarget={document.querySelector("body")}
-    />
-  );
+	return (
+		<Select
+			closeMenuOnSelect={false}
+			components={animatedComponents}
+			isMulti
+			defaultValue={[options[0]]}
+			value={selectedObjects}
+			onChange={onSelect}
+			options={options}
+			noOptionsMessage={() => 'No Options'}
+			placeholder="Select..."
+			className={'select-default ' + className}
+			menuPortalTarget={document.querySelector('body')}
+		/>
+	);
 }
