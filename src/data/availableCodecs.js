f33d761e48b574a0d1395f11e77fb99b2d7b35fe
HasibullahMahmood <HasibullahMahmood@gmail.com>
2021-05-09 16:05:10 +0300

    two files deleted

--- src/data/availableCodecs.js
+++
@@ -1,8 +0,0 @@
-const codecs = [
-  { label: "av1", value: 21 },
-  { label: "h264", value: 91 },
-  { label: "libx265", value: 102 },
-  { label: "mpeg4", value: 155 },
-];
-
-export default codecs;
--- src/data/availableFormats.js
+++
@@ -1,9 +0,0 @@
-const formats = [
-  { label: "mp4", value: 0 },
-  { label: "webm", value: 2 },
-  { label: "ogg", value: 3 },
-  { label: "hls", value: 1 },
-  { label: "dash", value: 4 },
-];
-
-export default formats;
